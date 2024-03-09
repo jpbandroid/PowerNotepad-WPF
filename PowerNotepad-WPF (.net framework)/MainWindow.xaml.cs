5c628e2f063e7b1f5d1eabbf2f469108b7d92a99
jpb <jakub.baranski.33@gmail.com>
2024-03-09 10:44:07 +0000

    add UI

--- PowerNotepad-WPF (.net framework)/MainWindow.xaml.cs
+++ PowerNotepad-WPF (.net framework)/MainWindow.xaml.cs
@@ -12,17 +12,108 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Fluent;
+using ScintillaNET.WPF;
+using ScintillaNET;
 
 namespace PowerNotepad_WPF__.net_framework_
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    /// 
+
+    public partial class MainWindow : RibbonWindow
     {
+        bool isDebug;
+        private const string NEW_DOCUMENT_TEXT = "Untitled";
+        private const int LINE_NUMBERS_MARGIN_WIDTH = 30; // TODO - don't hardcode this
+
+        /// <summary>
+        /// the background color of the text area
+        /// </summary>
+        private const int BACK_COLOR = 0x2A211C;
+
+        /// <summary>
+        /// default text color of the text area
+        /// </summary>
+        private const int FORE_COLOR = 0xB7B7B7;
+
+        /// <summary>
+        /// change this to whatever margin you want the line numbers to show in
+        /// </summary>
+        private const int NUMBER_MARGIN = 1;
+
+        /// <summary>
+        /// change this to whatever margin you want the bookmarks/breakpoints to show in
+        /// </summary>
+        private const int BOOKMARK_MARGIN = 2;
+
+        private const int BOOKMARK_MARKER = 2;
+
+        /// <summary>
+        /// change this to whatever margin you want the code folding tree (+/-) to show in
+        /// </summary>
+        private const int FOLDING_MARGIN = 3;
+
+        /// <summary>
+        /// set this true to show circular buttons for code folding (the [+] and [-] buttons on the margin)
+        /// </summary>
+        private const bool CODEFOLDING_CIRCULAR = false;
         public MainWindow()
         {
             InitializeComponent();
+            InitNumberMargin(scintillatext);
+        }
+
+        private void About(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void showinsiderinfo(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void update(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InitNumberMargin(ScintillaWPF ScintillaNet)
+        {
+            ScintillaNet.Styles[ScintillaNET.Style.LineNumber].BackColor = IntToColor(BACK_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.LineNumber].ForeColor = IntToColor(FORE_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.IndentGuide].ForeColor = IntToColor(FORE_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.IndentGuide].BackColor = IntToColor(BACK_COLOR);
+
+            var nums = ScintillaNet.Margins[NUMBER_MARGIN];
+            nums.Width = LINE_NUMBERS_MARGIN_WIDTH;
+            nums.Type = MarginType.Number;
+            nums.Sensitive = true;
+            nums.Mask = 0;
+
+            ScintillaNet.MarginClick += TextArea_MarginClick;
+        }
+
+        private void TextArea_MarginClick(object sender, MarginClickEventArgs e)
+        {
+        }
+
+        public static System.Drawing.Color IntToColor(int rgb)
+        {
+            return System.Drawing.Color.FromArgb(255, (byte)(rgb >> 16), (byte)(rgb >> 8), (byte)rgb);
         }
     }
 }
