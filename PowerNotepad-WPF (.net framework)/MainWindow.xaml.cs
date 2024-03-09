ee2f06f893859dd2389f42b2290b8a4fd758f20d
jpb <jakub.baranski.33@gmail.com>
2024-03-09 11:10:50 +0000

    Enable C# lexer

--- PowerNotepad-WPF (.net framework)/MainWindow.xaml.cs
+++ PowerNotepad-WPF (.net framework)/MainWindow.xaml.cs
@@ -66,6 +66,7 @@ namespace PowerNotepad_WPF__.net_framework_
         {
             InitializeComponent();
             InitNumberMargin(scintillatext);
+            InitSyntaxColoring(scintillatext);
         }
 
         private void About(object sender, RoutedEventArgs e)
@@ -76,11 +77,13 @@ namespace PowerNotepad_WPF__.net_framework_
         private void OpenClick(object sender, RoutedEventArgs e)
         {
             Open();
+            InitSyntaxColoring(scintillatext);
         }
 
         private void SaveClick(object sender, RoutedEventArgs e)
         {
             Save();
+            InitSyntaxColoring(scintillatext);
         }
 
         private void showinsiderinfo(object sender, RoutedEventArgs e)
@@ -141,7 +144,7 @@ namespace PowerNotepad_WPF__.net_framework_
             scintillatext.Text = File.ReadAllText(filePath);
             //doc.Scintilla.UndoRedo.EmptyUndoBuffer();
             //doc.Scintilla.Modified = false;
-            Title = System.IO.Path.GetFileName(filePath);
+            Title = System.IO.Path.GetFileName(filePath) + " - PowerNotepad";
             FilePath = filePath;
             //incrementalSearcher.Scintilla = doc.Scintilla;
 
@@ -180,6 +183,48 @@ namespace PowerNotepad_WPF__.net_framework_
         {
             get { return _filePath; }
             set { _filePath = value; }
+        }
+
+        private void InitSyntaxColoring(ScintillaWPF ScintillaNet)
+        {
+            // Configure the default style
+            ScintillaNet.StyleResetDefault();
+            ScintillaNet.Styles[ScintillaNET.Style.Default].Font = "Consolas";
+            ScintillaNet.Styles[ScintillaNET.Style.Default].Size = 10;
+            ScintillaNet.Styles[ScintillaNET.Style.Default].BackColor = IntToColor(0x212121);
+            ScintillaNet.Styles[ScintillaNET.Style.Default].ForeColor = IntToColor(0xFFFFFF);
+            ScintillaNet.Styles[ScintillaNET.Style.LineNumber].BackColor = IntToColor(BACK_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.LineNumber].ForeColor = IntToColor(FORE_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.IndentGuide].ForeColor = IntToColor(FORE_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.IndentGuide].BackColor = IntToColor(BACK_COLOR);
+            ScintillaNet.StyleClearAll();
+
+            // Configure the CPP (C#) lexer styles
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Identifier].ForeColor = IntToColor(0xD0DAE2);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Comment].ForeColor = IntToColor(0xBD758B);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.CommentLine].ForeColor = IntToColor(0x40BF57);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.CommentDoc].ForeColor = IntToColor(0x2FAE35);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Number].ForeColor = IntToColor(0xFFFF00);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.String].ForeColor = IntToColor(0xFFFF00);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Character].ForeColor = IntToColor(0xE95454);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Preprocessor].ForeColor = IntToColor(0x8AAFEE);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Operator].ForeColor = IntToColor(0xE0E0E0);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Regex].ForeColor = IntToColor(0xff00ff);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.CommentLineDoc].ForeColor = IntToColor(0x77A7DB);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Word].ForeColor = IntToColor(0x48A8EE);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.Word2].ForeColor = IntToColor(0xF98906);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.CommentDocKeyword].ForeColor = IntToColor(0xB3D991);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.CommentDocKeywordError].ForeColor = IntToColor(0xFF0000);
+            ScintillaNet.Styles[ScintillaNET.Style.Cpp.GlobalClass].ForeColor = IntToColor(0x48A8EE);
+            ScintillaNet.Styles[ScintillaNET.Style.LineNumber].BackColor = IntToColor(BACK_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.LineNumber].ForeColor = IntToColor(FORE_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.IndentGuide].ForeColor = IntToColor(FORE_COLOR);
+            ScintillaNet.Styles[ScintillaNET.Style.IndentGuide].BackColor = IntToColor(BACK_COLOR);
+
+            ScintillaNet.Lexer = Lexer.Cpp;
+
+            ScintillaNet.SetKeywords(0, "class extends implements import interface new case do while else if for in switch throw get set function var try catch finally while with default break continue delete return each const namespace package include use is as instanceof typeof author copy default deprecated eventType example exampleText exception haxe inheritDoc internal link mtasc mxmlc param private return see serial serialData serialField since throws usage version langversion playerversion productversion dynamic private public partial static intrinsic internal native override protected AS3 final super this arguments null Infinity NaN undefined true false abstract as base bool break by byte case catch char checked class const continue decimal default delegate do double descending explicit event extern else enum false finally fixed float for foreach from goto group if implicit in int interface internal into is lock long new null namespace object operator out override orderby params private protected public readonly ref return switch struct sbyte sealed short sizeof stackalloc static string select this throw true try typeof uint ulong unchecked unsafe ushort using var virtual volatile void while where yield");
+            ScintillaNet.SetKeywords(1, "void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path File System Windows Forms ScintillaNET");
         }
     }
 }
