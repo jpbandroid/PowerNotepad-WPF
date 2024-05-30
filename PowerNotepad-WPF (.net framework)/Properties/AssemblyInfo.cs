cb35466b5884daf34b5abcc7c762d1030ee252c6
jpb <jakub.baranski.33@gmail.com>
2024-05-30 09:26:33 +0100

    Update version

--- PowerNotepad-WPF (.net framework)/Properties/AssemblyInfo.cs
+++ PowerNotepad-WPF (.net framework)/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("16.0.14430.20270")]
-[assembly: AssemblyFileVersion("16.0.14430.20270")]
+[assembly: AssemblyVersion("16.0.14527.20226")]
+[assembly: AssemblyFileVersion("16.0.14527.20226")]
